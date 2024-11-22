e1659fb7e94596b09f2dfec68f76e85f1aa9e1f5
Bobby <bobur0218programmer@gmail.com>
2024-11-22 10:54:58 +0500

    update: app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cors({
   origin: "*", methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS"
 }));
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "We got your request" });
 });
 
