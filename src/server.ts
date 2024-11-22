bd2735b2493545c4db11fdc460dcfb439c6d33e2
Bobby <bobur0218programmer@gmail.com>
2024-11-22 11:30:11 +0500

    i hate developing fcckkkkk

--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,3 @@ dbConnection();
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
-
-// mongoose.connect(env.MONGODB_URL)
-//   .then(() => {
-//     console.log("Database connected");
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on port: ${PORT}`);
-//     });
-//   })
-//   .catch(console.error);
